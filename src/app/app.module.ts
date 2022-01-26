10c774695f0686897f01e85799e6e2934519cdac
Renée Gallison <reneegallison@Renees-MBP.lan>
2022-01-25 18:41:43 -0800

    getting autocomplete to work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ModalComponent } from './core/modal/modal.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { PatientFormComponent } from './patients/patient-form/patient-form.component';
 
 @NgModule({
@@ -31,6 +32,7 @@ import { PatientFormComponent } from './patients/patient-form/patient-form.compo
     MatButtonModule,
     MatDialogModule,
     MatInputModule,
+    MatAutocompleteModule,
     BrowserAnimationsModule
   ],
   providers: [],
