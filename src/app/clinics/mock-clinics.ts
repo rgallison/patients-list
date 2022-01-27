d7de7f8c36c9c474012387a669fb97acd5b7fbb2
Renée Gallison <reneegallison@Renees-MBP.lan>
2022-01-27 14:39:28 -0800

    I swore I added this

---
+++ src/app/clinics/mock-clinics.ts
@@ -0,0 +1,11 @@
+import { Clinic } from './clinic';
+
+export const CLINICS: Clinic[] = [
+    { name: 'Great River Community Hospital' },
+    { name: 'Harmony Clinic' },
+    { name: 'Pearl River Hospital' },
+    { name: 'Morningside Hospital' },
+    { name: 'Serenity Hospital' },
+    { name: 'Desert Springs Medical Clinic' },
+    { name: 'Summerstone Medical Clinic' }
+]
